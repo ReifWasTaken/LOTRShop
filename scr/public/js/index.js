e4e976f26f9b372d4bb339f7376092c304f8c0a1
Reif <gregodelgado182@gmail.com>
2023-06-27 21:15:26 -0300

    se esta trabajando sobre añadir un producto

--- scr/public/js/index.js
+++ scr/public/js/index.js
@@ -1,76 +1,23 @@
-const socket = io();
-const formu = document.getElementById("formu")
-const title = document.getElementById("title")
-const description = document.getElementById("description")
-const price = document.getElementById("price")
-const thumbnail = document.getElementById("thumbnail")
-const code = document.getElementById("code")
-const category = document.getElementById("category")
-const stock = document.getElementById("stock")
-
-
-socket.on("error", (error)=> {
-    alert(error.msg)
-})
-
-socket.on("all_products", (data)=>{
-    const product = document.getElementById("product")
-
-    product.innerHTML =  `${data.map(e =>
-        
-    `
-    <ul>
-        <h1>Product: ${e.title}</h1>
-        <li>ID: ${e.id}</li>
-        <li>description:${e.description}</li>
-        <li>price:${e.price}</li>
-        <li>thumbnail:${e.thumbnail}</li>
-        <li>stock: ${e.stock}</li>
-        <button id="delete" type="submit" onclick="deleteButton(${e.id})">X</button>
-    </ul>`
-
-        ).join("")}`
-});
-
-formu.addEventListener("submit", (e)=>{
-    e.preventDefault();
-
-    const newProduct = {
-        
-        title: title.value,
-        description: description.value,
-        price: price.value,
-        thumbnail: thumbnail.value,
-        code: code.value,
-        category: category.value,
-        stock: stock.value,
-    }
-
-    title.value = ""
-    description.value = ""
-    price.value = ""
-    thumbnail.value = ""
-    code.value = ""
-    category.value = ""
-    stock.value = ""
-
-    socket.emit("new_product", newProduct);
-});
-
-     deleteButton = (IDProd)=> {
-
-        socket.emit("delete_product", IDProd);
-    }
-
-     AddToCart = (IDProd) => {
-
-        localStorage.setItem("IDCart", "6494e71d6f254e090bcc0fd3")
-
-        fetch(`/carts/${IDCart}/products/${IDProd}`, {
-            method: "PUT"})
-        };
-
-    
-    
-
-
+addToCart(()=>{
+  console.log("first")
+}) 
+/*         async function addToCart(pid) {       
+          let cartId = localStorage.getItem('cartId')
+            if (!cartId) {
+              const response = await fetch('/api/carts/', {
+                method: 'post'
+              })
+              const body = await response.json()
+              localStorage.setItem('cartId', body.payload)
+              cartId = body.payload
+            }
+            const response = await fetch(`/api/carts/${cartId}/product/${pid}`, {
+              method: 'post'
+            })
+          
+            if (response.ok) {
+              alert('Producto agregado al carrito')
+            } else {
+              alert((await response.json()).error)
+            }
+          } */
